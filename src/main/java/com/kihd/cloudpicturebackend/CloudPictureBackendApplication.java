2651b7b744f0a495ae132e041c34367a7a6468f9
kihd <1684105955@qq.com>
2025-03-12 14:43:54 +0800

    后端初始化

---
+++ src/main/java/com/kihd/cloudpicturebackend/CloudPictureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kihd.cloudpicturebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudPictureBackendApplication.class, args);
+    }
+
+}
