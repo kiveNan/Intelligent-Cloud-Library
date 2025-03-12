ba6827c31d88ef0d14da035ad12eec854c193dfa
kihd <1684105955@qq.com>
2025-03-12 14:50:24 +0800

    后端初始化-mybatisplus引入

--- src/main/java/com/kihd/cloudpicturebackend/CloudPictureBackendApplication.java
+++ src/main/java/com/kihd/cloudpicturebackend/CloudPictureBackendApplication.java
@@ -1,9 +1,11 @@
 package com.kihd.cloudpicturebackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.kihd.cloudpicturebackend.mapper")
 public class CloudPictureBackendApplication {
 
     public static void main(String[] args) {
