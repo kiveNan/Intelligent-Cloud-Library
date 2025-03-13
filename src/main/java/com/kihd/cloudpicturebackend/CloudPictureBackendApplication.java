dd92423a7d2ab10ba0b6bd3f8334b5518af710f4
kihd <1684105955@qq.com>
2025-03-13 11:32:01 +0800

    后端初始化-切面引入

--- src/main/java/com/kihd/cloudpicturebackend/CloudPictureBackendApplication.java
+++ src/main/java/com/kihd/cloudpicturebackend/CloudPictureBackendApplication.java
@@ -3,9 +3,11 @@ package com.kihd.cloudpicturebackend;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @MapperScan("com.kihd.cloudpicturebackend.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class CloudPictureBackendApplication {
 
     public static void main(String[] args) {
