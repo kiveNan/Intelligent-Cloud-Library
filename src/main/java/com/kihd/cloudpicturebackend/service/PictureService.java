ee181dc286a9b9512b1771bc00930d499417105c
kihd <1684105955@qq.com>
2025-03-18 16:31:14 +0800

    图片管理

--- src/main/java/com/kihd/cloudpicturebackend/service/PictureService.java
+++ src/main/java/com/kihd/cloudpicturebackend/service/PictureService.java
@@ -1,11 +1,16 @@
 package com.kihd.cloudpicturebackend.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.kihd.cloudpicturebackend.model.dto.picture.PictureQueryRequest;
 import com.kihd.cloudpicturebackend.model.dto.picture.PictureUploadRequest;
 import com.kihd.cloudpicturebackend.model.entity.Picture;
 import com.kihd.cloudpicturebackend.model.entity.User;
 import com.kihd.cloudpicturebackend.model.vo.picture.PictureVO;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
 * @author Nan Liu
@@ -26,4 +31,35 @@ public interface PictureService extends IService<Picture> {
                             PictureUploadRequest pictureUploadRequest,
                             User loginUser);
 
+    /**
+     * 获取查询条件
+     *
+     * @param pictureQueryRequest
+     * @return
+     */
+    QueryWrapper<Picture> getQueryWrapper(PictureQueryRequest pictureQueryRequest);
+
+    /**
+     * 获取图片封装
+     *
+     * @param picture
+     * @param request
+     * @return
+     */
+    PictureVO getPictureVO(Picture picture, HttpServletRequest request);
+
+    /**
+     * 分页获取图片封装
+     *
+     * @param picturePage
+     * @param request
+     * @return
+     */
+    Page<PictureVO> getPictureVOPage(Page<Picture> picturePage, HttpServletRequest request);
+
+    /**
+     * 图片数据校验
+     * @param picture
+     */
+    void validPicture(Picture picture);
 }
